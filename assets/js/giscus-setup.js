fb02b89f310ec50e15948e98f5f76b269a3ec724
yurekami <crystalinecohomology@gmail.com>
2026-02-14 14:23:22 +0000

    Deploying to gh-pages from @ yurekami/yurekami.github.io@691054935e8b69ad93b858dd8a28a34ee62c2868 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -19,9 +19,9 @@ function determineGiscusTheme() {
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
     "data-repo": "yurekami/yurekami.github.io",
-    "data-repo-id": "",
-    "data-category": "Comments",
-    "data-category-id": "",
+    "data-repo-id": "R_kgDORL1QJg",
+    "data-category": "General",
+    "data-category-id": "DIC_kwDORL1QJs4C2bXF",
     "data-mapping": "title",
     "data-strict": "1",
     "data-reactions-enabled": "1",
